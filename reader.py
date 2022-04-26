44b475213dcd533c1c330d651c7152153201f5b2
Jakub <grabarczyk.kuba@gmail.com>
2022-04-26 23:12:37 +0200

    reader.py created

---
+++ reader.py
@@ -0,0 +1,30 @@
+import sys
+from classes import CSVHandler, PICKLEHandler, JSONHandler, temporary_list
+
+file_in = sys.argv[1]
+file_out = sys.argv[2]
+location_x = int(sys.argv[3])
+location_y = int(sys.argv[4])
+command_change = sys.argv[5]
+
+
+pickle_file = PICKLEHandler(file_in, file_out)
+json_file = JSONHandler(file_in, file_out)
+csv_file = CSVHandler(file_in, file_out)
+
+if file_in[-3:] == 'csv':
+    csv_file.load_file()
+if file_in[-4:] == 'json':
+    json_file.load_file()
+if file_in[-6:] == 'pickle':
+    pickle_file.load_file()
+
+print(temporary_list)
+temporary_list[location_x][location_y] = command_change
+
+if file_out[-3:] == 'csv':
+    csv_file.dump_file()
+if file_out[-4:] == 'json':
+    json_file.dump_file()
+if file_out[-6:] == 'pickle':
+    pickle_file.dump_file()
