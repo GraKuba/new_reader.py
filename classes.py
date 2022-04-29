a8fa863352c34d3f99397dbb424ddf255ca9e4ac
Jakub <grabarczyk.kuba@gmail.com>
2022-04-29 20:29:11 +0200

    Problem with json and pickle files fixed

--- classes.py
+++ classes.py
@@ -2,11 +2,11 @@ import csv
 import json
 import pickle
 
-temporary_list = []
+ls = []
 
 
 class DownloadAndSave:
-    def __init__(self, file_download, file_save):
+    def __init__(self, file_download: str, file_save: str):
         self.file_download = file_download
         self.file_save = file_save
 
@@ -14,27 +14,36 @@ class DownloadAndSave:
         with open(self.file_download, 'r') as f:
             reader = csv.reader(f)
             for line in reader:
-                temporary_list.append(line)
-        return temporary_list
+                ls.append(line)
+            return ls
 
-    def download_pkl_json(self, argument, file_type):
+    def download_pkl_json(self, argument: str):
+        if argument == '':
+            file_type = json
+        else:
+            file_type = pickle
         with open(self.file_download, 'r' + argument) as f:
-            file_type.load(temporary_list, f)
-        return temporary_list
+            data = file_type.load(f)
+            for line in data:
+                ls.append(line)
+            return ls
 
     def dump_csv(self):
         with open(self.file_save, "w") as f:
             writer = csv.writer(f)
-            for line in temporary_list:
+            for line in ls:
                 writer.writerow(line)
 
-    def dump_pkl_json(self, argument, file_type):
+    def dump_pkl_json(self, argument: str):
+        if argument == '':
+            file_type = json
+        else:
+            file_type = pickle
         with open(self.file_save, 'w' + argument) as f:
-            file_type.dump(temporary_list, f)
+            file_type.dump(ls, f)
 
 
 class CSVHandler(DownloadAndSave):
-    file_type = csv
 
     def load_file(self):
         return super().download_csv()
@@ -45,19 +54,17 @@ class CSVHandler(DownloadAndSave):
 
 class JSONHandler(DownloadAndSave):
 
-    def load_file(self):
-        super().download_pkl_json('', json)
+    def load_file(self, argument):
+        super().download_pkl_json(argument)
 
-    def dump_file(self):
-        super().dump_pkl_json('', json)
+    def dump_file(self, argument):
+        super().dump_pkl_json(argument)
 
 
 class PICKLEHandler(DownloadAndSave):
 
-    def load_file(self):
-        super().download_pkl_json('b', pickle)
+    def load_file(self, argument):
+        super().download_pkl_json(argument)
 
-    def dump_file(self):
-        super().download_pkl_json('b', pickle)
-
-
+    def dump_file(self, argument):
+        super().dump_pkl_json(argument)
--- reader.py
+++ reader.py
@@ -1,5 +1,5 @@
 import sys
-from classes import CSVHandler, PICKLEHandler, JSONHandler, temporary_list
+from classes import CSVHandler, PICKLEHandler, JSONHandler, ls
 
 file_in = sys.argv[1]
 file_out = sys.argv[2]
@@ -7,24 +7,27 @@ location_x = int(sys.argv[3])
 location_y = int(sys.argv[4])
 command_change = sys.argv[5]
 
-
 pickle_file = PICKLEHandler(file_in, file_out)
 json_file = JSONHandler(file_in, file_out)
 csv_file = CSVHandler(file_in, file_out)
 
 if file_in[-3:] == 'csv':
     csv_file.load_file()
-if file_in[-4:] == 'json':
-    json_file.load_file()
-if file_in[-6:] == 'pickle':
-    pickle_file.load_file()
+elif file_in[-4:] == 'json':
+    json_file.load_file('')
+elif file_in[-6:] == 'pickle':
+    pickle_file.load_file('b')
+else:
+    print("Wrong file type selected.")
 
-print(temporary_list)
-temporary_list[location_x][location_y] = command_change
+ls[location_x][location_y] = command_change
+print(ls)
 
 if file_out[-3:] == 'csv':
     csv_file.dump_file()
-if file_out[-4:] == 'json':
-    json_file.dump_file()
-if file_out[-6:] == 'pickle':
-    pickle_file.dump_file()
+elif file_out[-4:] == 'json':
+    json_file.dump_file('')
+elif file_out[-6:] == 'pickle':
+    pickle_file.dump_file('b')
+else:
+    print("Wrong file type selected.")
